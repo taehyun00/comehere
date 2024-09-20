4435e728eff6eb253b77eecf0863f81d2eb079ac
김태현 <143389711+taehyun00@users.noreply.github.com>
2024-09-20 23:18:06 +0900

    Update messages.js

--- utils/messages.js
+++ utils/messages.js
@@ -1,11 +1,10 @@
-const moment = require('moment-timezone');
+const moment = require('moment');
 
 function formatMessage(name, text) {
     return {
         name,
         text,
-        // 'Asia/Seoul' 시간을 사용하여 시간을 설정 (필요에 따라 다른 시간대로 변경 가능)
-        time: moment().tz('Asia/Seoul').format('h:mm a')
+        time: moment.utc().format('h:mm a')  // UTC 시간으로 설정
     };
 }
 
